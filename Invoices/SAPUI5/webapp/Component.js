23ce865ba381461044f19991f5cc2452c656bc09
Cesar-A-Ceh <cesaracm95@hotmail.com>
2024-03-21 22:34:15 +0000

    Commit_Unidad9

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -1,17 +1,19 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/models",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, models, ResourceModel) {
-
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
+], 
+/**
+ * @param {sap.ui.core.UIComponent} UIComponent 
+ * @param {sap.ui.model.resource.ResourceModel} ResourceModel 
+ */
+function (UIComponent, models, ResourceModel,HelloDialog) {
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
-
             metadata:{
                 manifest:"json"
             },
-
             init: function () {
-
                 //Call the init function of the this parent
                 UIComponent.prototype.init.apply(this, arguments);
 
@@ -24,10 +26,18 @@ sap.ui.define([
                 });
 
                 this.setModel(i18nModel, "i18n");
-            }
-        
-        });
 
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            },
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+
+            },
+            openHelloDialog:function () {
+                this._helloDialog.open();
+            }
+        });
     }
 );
     
--- Invoices/SAPUI5/webapp/controller/App.controller.js
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -1,25 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ],
-    function (Controller, MessageToast) {//, models, ResourceModel
+    function (Controller) {
         "use strict"
+
         return Controller.extend("logaligroup.SAPUI5.controller.App", {
             onInit: function () {
-                /*this.getView().setModel(models.createRecipient());
-                var i18nModel = new ResourceModel({
-                    bundleName:"logaligroup.SAPUI5.i18n.i18n"
-                });
-                this.getView().setModel(i18nModel,"i18n");*/
-
+                
             },
-            onShowHello: function () {
-                //MessageToast.show("Hello World");
-                var sHello = this.getView().getModel("i18n").getResourceBundle().getText("say Hello");
-                var sName = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = sHello.concat(" ").concat(sName);
 
-                MessageToast.show(sMsg);
+            onOpenDialogHeader: function () {
+                this.getOwnerComponent().openHelloDialog();
             }
+
         });
-    });
+    }
+);
